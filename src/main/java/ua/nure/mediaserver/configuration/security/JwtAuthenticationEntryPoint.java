65a302733c0ed08b668a779e09748f90f6e8a4a4
Gleb Bruner <glebbruner1996@gmail.com>
2018-06-06 16:30:14 +0300

    small change

--- src/main/java/ua/nure/mediaserver/configuration/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/ua/nure/mediaserver/configuration/security/JwtAuthenticationEntryPoint.java
@@ -15,8 +15,10 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        //response.setContentType(MediaType.APPLICATION_JSON_VALUE); //todo maybe do some good response JSON
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Sorry, You're not authorized to access this resource." );
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"error\":\"You are not authorized\"}"); //todo maybe make it more neat
     }
 
 
